470eeb99580cbcfb3ac57dba97398d8e96f29682
Furka <Furka@FURKAN>
2024-08-25 15:04:54 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Same_Picture
+{
+    public partial class Form1 : Form
+    {
+        public int screen_width;
+        public int screen_height;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load+=Form1_Load;
+
+            button1.MouseDown+=Button1_MouseDown;
+            button1.Click+=Button1_Click;
+
+            button2.Click+=Button2_Click;
+            button2.Enabled=true;
+           
+
+            //string[] strings1 = { "picture1", "picture2", "picture3", "picture4" };
+            //screen_width = this.Width;
+            screen_width = Screen.PrimaryScreen.Bounds.Width;
+            screen_height = Screen.PrimaryScreen.Bounds.Height;
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button1.Text = "OK";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(200,200);
+            this.Width = screen_width-300;
+            this.Height = screen_height-300;
+            //this.BackColor = SystemColors.Window;
+            //this.BackColor = SystemColors.GrayText;
+            this.BackColor = Color.ForestGreen;
+            this.FormBorderStyle = FormBorderStyle.None;
+            //this.WindowState = FormWindowState.Maximized;
+             
+
+
+            //string[] strings1 = new string[] { "a1", "a2" };
+            string[] strings1 = new string[4];
+            strings1 = new string[] { "picture1", "picture2", "picture3", "picture4" };
+
+            //label1.Text = strings1[0];
+            //label1.Text = screen_width.ToString();
+            //label2.Text = screen_height.ToString();
+            
+            label1.Text = string.Empty;
+            label2.Text = string.Empty;
+            
+            button1.Location = new Point(this.Width - 100,10);
+            button1.Text = "Form Close";
+            //button1.Text = button2.Visible.ToString();
+
+            Form2 form2 = new Form2();
+            form2.StartPosition = FormStartPosition.Manual;
+            form2.Location = new Point(50,50);
+            form2.Width = screen_width - 100;
+            form2.Height = screen_height - 100;
+            //form2.WindowState = FormWindowState.Maximized;
+            //form2.BackColor = this.BackColor;
+            form2.BackColor =SystemColors.Desktop;
+            form2.FormBorderStyle = FormBorderStyle.None;
+            form2.Show();
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Same_Picture
+{
+    public partial class Form2 : Form
+    {
+       
+        public Form2()
+        {
+            InitializeComponent();
+            button1.BackColor =Color.Orange;
+            button1.Text = this.Width.ToString(); // 816 pixel
+
+            this.Load +=Form2_Load;
+            button1.Click+=Button1_Click;
+            button2.Click+=Button2_Click;
+            button3.Click+=Button3_Click;
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            this.TopLevel = false;
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //button1.Text = this.Width.ToString(); // 1804 pixel
+            button1.Text ="Close";
+
+            button1.Left = this.Width - 150;
+            button1.Top = 10;
+
+            button2.Left = this.Width - 150;
+            button2.Top = 50;
+            button2.Text ="Front";
+
+            button3.Left = this.Width - 150;
+            button3.Top = 100;
+            button3.Text ="Back";
+
+            
+            
+
+         
+
+        }
+    }
+}
